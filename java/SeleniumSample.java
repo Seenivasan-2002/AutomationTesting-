c5f664735bd6c1cf28311140956b60a875083bbe
Seenivasan-2002 <seenivasansanmugam456@gmail.com>
2024-11-27 04:00:47 +0530

    Delete java directory

--- java/SeleniumSample.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class SeleniumSample {
-	public static void main(String[] args) {
-		ChromeDriver driver=new ChromeDriver();
-		driver.get("http://leaftaps.com/opentaps/control/main");
-		}
-
-}
